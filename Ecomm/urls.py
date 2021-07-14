93b6a63e2f59779600257fddb3fae72e0c1d6d16
harbindsingh <60515499+harbindsingh@users.noreply.github.com>
2021-07-14 12:38:25 +0530

    Update urls.py

--- Ecomm/urls.py
+++ Ecomm/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.Index, name="home"),
+    path('', views.IndexView, name="home"),
     path('product/<int:id>/<str:name>/', views.ProductPage, name='product' ),
     path('categories/', views.AllCategoryPage, name='allCategories' ),
     path('<str:category>/',views.SingleCategoryPage.as_view(), name='category'),
